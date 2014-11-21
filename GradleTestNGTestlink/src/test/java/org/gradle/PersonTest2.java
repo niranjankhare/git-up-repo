d8c8e6ee0cb79c24625791d58e95ba52816c0ea4
niranjankhare <nkh@ums.no>
2014-11-21 18:56:32 +0530

    adding second class file

---
+++ GradleTestNGTestlink/src/test/java/org/gradle/PersonTest2.java
@@ -0,0 +1,22 @@
+package org.gradle;
+
+import static org.testng.AssertJUnit.assertEquals;
+import org.testng.annotations.Test;
+@Test
+public class PersonTest2 {
+   
+	@Test (priority = 2)
+    public void canConstructAPersonWithAName1() {
+        Person person = new Person("Larry");
+        assertEquals("Larry", person.getName());
+        assertEquals("Boolean test", true, false);
+        System.out.println("Test complete!!");
+    }
+	@Test (priority = 1)
+    public void canConstructAPersonWithAName2() {
+        Person person = new Person("Larry");
+        assertEquals("Larry", person.getName());
+//        assertEquals("Boolean test", true, false);
+        System.out.println("Test complete!!");
+    }
+}
